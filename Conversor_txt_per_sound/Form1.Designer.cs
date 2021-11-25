350490ae8a8776ba52433de5a67c9bafdcf4155a
guimafell <60404761+guimafell@users.noreply.github.com>
2021-11-25 01:02:38 -0300

    Código para a conversão de texto para áudio.

---
+++ Conversor_txt_per_sound/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Conversor_txt_per_sound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtForAudio = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o texto";
+            // 
+            // TxtForAudio
+            // 
+            this.TxtForAudio.Location = new System.Drawing.Point(10, 33);
+            this.TxtForAudio.Multiline = true;
+            this.TxtForAudio.Name = "TxtForAudio";
+            this.TxtForAudio.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtForAudio.Size = new System.Drawing.Size(327, 335);
+            this.TxtForAudio.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 106);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reproduzir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(352, 268);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 97);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 377);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtForAudio);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Convertendo txt para audio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtForAudio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Conversor_txt_per_sound/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Conversor_txt_per_sound
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        private static SpeechSynthesizer synthesizer;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string convesor = TxtForAudio.Text;
+            synthesizer = new SpeechSynthesizer();
+
+            if (convesor!="")
+            {
+                
+                synthesizer.SpeakAsync(convesor);
+
+                
+            }
+            else
+            {
+                string aviso = "Escreva um texto para eu poder falar";
+                synthesizer.SpeakAsync(aviso);
+                MessageBox.Show(aviso);
+            }
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TxtForAudio.Text = "";
+        }
+        
+    }
+}
